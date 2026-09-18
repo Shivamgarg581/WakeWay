f3dcf2f9add1e6a9f72e4bedb766d6d1df46de2e
Shivamgarg581 <shivgarg184@gmail.com>
2026-09-18 14:19:24 +0530

    Update Android SDK to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,12 @@ plugins {
 
 android {
     namespace = "com.wakeway.app"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.wakeway.app"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "0.1.0"
 
