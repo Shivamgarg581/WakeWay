a71549e18e93ce9e394df7a8f4d8b39129a65851
Shivamgarg581 <shivgarg184@gmail.com>
2026-09-19 14:49:37 +0530

    Bump WakeWay app version for improved build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.wakeway.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
 
         buildConfigField("String", "BACKEND_URL", "\"" + localProps.getProperty("WAKEWAY_BACKEND_URL", "") + "\"")
         buildConfigField("String", "SUPABASE_URL", "\"" + localProps.getProperty("SUPABASE_URL", "") + "\"")
