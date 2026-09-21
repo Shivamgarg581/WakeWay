b5124bda88c1f1d0ac1d890690f8ce2fe34d866c
Shivamgarg581 <shivgarg184@gmail.com>
2026-09-21 10:35:24 +0530

    Pin Compose UI test dependency to current stable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.19.0")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.12.1")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test:runner:1.7.0")
 }
